600536b2b63f790e464143642a36505ebdc07b8a
Farida Eid <farida.eid@aucgypt.edu>
2017-07-23 23:05:14 +0200

    Calculator version after lecture 1

---
+++ cs193-Calculator/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  cs193-Calculator
+//
+//  Created by Farida Eid on 7/23/17.
+//  Copyright © 2017 Farida Eid. All rights reserved.
+//
+
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Michel Deiman on 16/02/2017.
+//  Copyright © 2017 Michel Deiman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+    
+}
+
+
